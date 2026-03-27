0b0def1fd4fbfff5e6af0b2dac3c640c66ebbc2b
WAHEEDIANHO <waheedianho65@gmail.com>
2026-03-27 16:16:30 +0100

    feature: payment-modify

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -5,9 +5,11 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
 import { PaymentMethod } from './entities/payment-method.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { UtilsModule } from 'src/utils/utils.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment, PaymentMethod]), ConfigModule],
+  imports: [TypeOrmModule.forFeature([Payment, PaymentMethod]), ConfigModule, JwtModule, UtilsModule],
   controllers: [PaymentController],
   providers: [PaymentService],
   exports: [PaymentService],
